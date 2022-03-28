431daa8988fe5ad77021fc41aff3660462c4d3c9
rashidgafurov <nsd7c4@gmail.com>
2022-03-28 15:44:48 -0400

    homework 9, assignment 6

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -3,6 +3,11 @@ package com.RashidGafurov_INC_LOL;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Assignment 6
+        Author pushkinAS = new Author("Alexander", "Pushkin");
+        Author gogolNV = new Author("Nikolai", "Gogol");
+
+        Book mertvyeDushi = new Book("Mertvye Dushi", gogolNV, 1842);
+        Book ruslanILudmila = new Book("Ruslan i Ludmila", pushkinAS, 1820);
     }
 }
